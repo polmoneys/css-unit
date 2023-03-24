15dcc63df79e92ed448e7bab5836c91f48200c72
Pol Moneys <p0lm0n3ys@gmail.com>
2023-03-24 16:56:09 +0100

    chores

--- src/classNames.ts
+++ src/classNames.ts
@@ -7,5 +7,16 @@ const theme = {
     ERROR_UNIT: 'error-unit',
 };
 
+const classes = {
+    UNIT: 'unit',
+    GROUP: 'group',
+    LIGHT_UNIT: 'light-unit',
+    DARK_UNIT: 'dark-unit',
+    FOCUS_UNIT: 'focus-unit',
+    ERROR_UNIT: 'error-unit',
+} as const;
+
+export type ClassNames = keyof typeof classes;
+
 export type Theme = typeof theme;
 export default theme;
